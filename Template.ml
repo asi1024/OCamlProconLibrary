67ff0b26ed8999e547ead34aef9e7779cba81536
Akifumi Imanishi <imanishi@fos.kuis.kyoto-u.ac.jp>
2017-04-04 17:30:54 +0900

    Update templates

--- Template.ml
+++ Template.ml
@@ -9,16 +9,16 @@ let pf = Printf.printf
 let sf = Scanf.scanf
 let ssf = Scanf.sscanf
 
-let (|>) x f = f x
-let (@@) f x = f x
-
-let id x = x
-let id2 x y = (x, y)
-let id3 x y z = (x, y, z)
-let id4 a b c d = (a, b, c, d)
-let id5 a b c d e = (a, b, c, d, e)
-
+let read_int () = sf "%d " (fun x -> x)
+let read_float () = sf "%f " (fun x -> x)
+let read_array read n = A.init n (fun _ -> read ())
 let err s = raise (Failure s)
 
-let inf = 1000000000
+let inf = int_of_float 1e18
 let eps = 1e-11
+
+module S = struct
+  include S
+  let of_array a = S.init (A.length a) (A.get a)
+  let to_array s = A.init (S.length s) (S.get s)
+end;;
